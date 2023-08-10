d54b6af4203979a2cb8706efb45384479ef9a049
Sean Cloudsdale <sean@cloudsdale.co.uk>
2023-08-10 08:52:56 +0100

    make repo

--- header.php
+++ header.php
@@ -25,28 +25,27 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'cloudsdale-master' ); ?></a>
 
+	
+
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$cloudsdale_master_description = get_bloginfo( 'description', 'display' );
-			if ( $cloudsdale_master_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $cloudsdale_master_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+
+			<div class="cm-nav shadow">
+				<div class="container cm-nav-inner">
+					<a class="menu-toggle " aria-controls="primary-menu" aria-expanded="false" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNav">
+					<div class="menu-icon shadow"></div></a>
+					<a class="navbar-brand shadow" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/logos/vanguard-logo-text.svg" alt="Vanguard Logo" style>
+					</a>
+					<div class="social-icons">
+						<a class="instagram-icon shadow" target="_blank" href=""></a>
+						<a class="twitter-icon shadow"  target="_blank" href=""></a>
+						<a class="facebook-icon shadow"  target="_blank" href=""></a>
+					</div>
+				</div>
+			</div>
+
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNav"><?php esc_html_e( 'Primary Menu', 'cloudsdale-master' ); ?></button>
 
 			<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNav" aria-labelledby="offcanvasNavLabel">
 				<div class="offcanvas-header">
--- template-carousel.php
+++ template-carousel.php
@@ -1,4 +1,34 @@
 <?php /* Template Name: Carosel */ get_header(); ?>
+
+<!-- Full Page Intro -->
+
+<div class="view full-page-intro z-depth-3">
+
+<div class="view full-page-intro" style="background-image:  url(<?php the_post_thumbnail_url(); ?>); background-repeat: no-repeat; background-position: center; background-size: cover;">
+ 
+		 </div>
+<!-- Mask & flexbox options-->
+
+<div class="mask d-flex justify-content-center align-items-center">
+
+  <!-- Content -->
+  <div class="container">
+
+
+
+  </div>
+  <!-- Content -->
+
+  
+
+</div>
+<!-- Mask & flexbox options-->
+
+</div>
+<!-- Full Page Intro --> 
+
+
+
 <section class="container">
 <div id="loopCarousel" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
